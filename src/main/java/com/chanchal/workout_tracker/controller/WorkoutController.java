ab713f785c753dc689140d0b4493a3afe9a783ec
Chanchal Verma <chnchlverma03@gmail.com>
2026-03-09 12:06:20 +0530

    Added PostgreSQL database support

--- src/main/java/com/chanchal/workout_tracker/controller/WorkoutController.java
+++ src/main/java/com/chanchal/workout_tracker/controller/WorkoutController.java
@@ -11,6 +11,7 @@ import java.util.List;
 @CrossOrigin
 public class WorkoutController {
 
+
     private final WorkoutService service;
 
     public WorkoutController(WorkoutService service) {
--- src/main/java/com/chanchal/workout_tracker/repository/WorkoutRepository.java
+++ src/main/java/com/chanchal/workout_tracker/repository/WorkoutRepository.java
@@ -3,5 +3,7 @@ package com.chanchal.workout_tracker.repository;
 import com.chanchal.workout_tracker.entity.Workout;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
 public interface WorkoutRepository extends JpaRepository<Workout, Long> {
 }
+
